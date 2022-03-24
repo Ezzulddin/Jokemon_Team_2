76a3423920ddf359f4e506a23f3aafcac2a5891f
Ezzulddin Al-Qaisi <10Al-QaisiE@student.smchull.org>
2022-03-24 09:50:57 +0000

    Deleted comments

---
+++ Jokemon_Team_2/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Jokemon_Team_2
+{
+    class InputManager
+    {
+        public void CheckKeys()
+        {
+            
+        }
+    }
+}
