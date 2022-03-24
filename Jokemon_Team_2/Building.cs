aeceaa07912ea2478bba57bc1b30fcb78c67f960
Jakub Kroczak <10KroczakJ@student.smchull.org>
2022-03-24 09:54:42 +0000

    Building Class

---
+++ Jokemon_Team_2/Building.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Jokemon_Team_2
+{
+    class Building : Sprite
+    {
+        bool isCollidable = true;
+        bool inInteractable = true;
+        public Building(Texture2D tex, Vector2 pos, Vector2 size) : base(tex, pos, size)
+        {
+        }
+    }
+
+}
