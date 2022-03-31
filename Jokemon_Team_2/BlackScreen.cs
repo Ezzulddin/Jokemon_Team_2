25b7ce5324b7663bbd73abf726e84a88174b3c95
Ezzulddin Al-Qaisi <10Al-QaisiE@student.smchull.org>
2022-03-31 10:05:01 +0100

    added black screen class

---
+++ Jokemon_Team_2/BlackScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Jokemon_Team_2
+{
+    class BlackScreen
+    {
+        Color blackSCreen;
+        public void LoadBlackSCreen(bool isBlack, GraphicsDevice GraphicsDevice)
+        {
+            blackSCreen = Color.Black;
+            if (isBlack == true)
+            {
+                GraphicsDevice.Clear(blackSCreen);
+            }
+            
+        }
+    }
+}
