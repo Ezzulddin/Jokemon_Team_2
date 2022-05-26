6a8636ce5903de22da0a0e065d6aa1ce1c3d64e5
Ezzulddin Al-Qaisi <10Al-QaisiE@student.smchull.org>
2022-05-26 13:15:31 +0100

    I want to die i fked up bad time

--- Jokemon_Team_2/Game1.cs
+++ Jokemon_Team_2/Game1.cs
@@ -69,7 +69,55 @@ namespace Jokemon_Team_2
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             loadContent = Content.Load<Texture2D>("Big_tree");
-            
+            //posX = 750;
+            //posY = 0;
+            //for (int i = 0; i < treeRow1.Length; i++)
+            //{
+            //    posY = 0 + (i * 80);
+            //    treeRow1[i] = new Tree(loadContent, new Vector2(posX, posY), new Vector2(60, 100), true);
+            //    treeObjects.Add(treeRow1[i]);
+            //}
+
+            //posX = 50;
+            //posY = 740;
+
+            //for (int i = 0; i < treeRow2.Length; i++)
+            //{
+            //    posX = 50 + (i * 50);
+
+            //    treeRow2[i] = new Tree(loadContent, new Vector2(posX, posY), new Vector2(60, 100), true);
+            //    treeObjects.Add(treeRow2[i]);
+            //}
+            //posX = 0;
+            //posY = 0;
+
+            //for (int i = 0; i < treeRow3.Length; i++)
+            //{
+            //    posX = 0 + (i * 50);
+
+            //    treeRow3[i] = new Tree(loadContent, new Vector2(posX, posY), new Vector2(60, 100), true);
+            //    treeObjects.Add(treeRow3[i]);
+            //}
+            //posX = 430;
+            //posY = 0;
+
+            //for (int i = 0; i < treeRow4.Length; i++)
+            //{
+            //    posX = 430 + (i * 45);
+
+            //    treeRow4[i] = new Tree(loadContent, new Vector2(posX, posY), new Vector2(60, 100), true);
+            //    treeObjects.Add(treeRow4[i]);
+            //}
+            //posX = 0;
+            //posY = 0;
+
+            //for (int i = 0; i < treeRow5.Length; i++)
+            //{
+            //    posY = 0 + (i * 80);
+
+            //    treeRow5[i] = new Tree(loadContent, new Vector2(posX, posY), new Vector2(60, 100), true);
+            //    treeObjects.Add(treeRow5[i]);
+            //}
 
             loadContent = Content.Load<Texture2D>("Player_M");
             player = new Player(loadContent, new Vector2(360, 380), new Vector2(35, 50),true);
